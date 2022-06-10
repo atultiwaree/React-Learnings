8f18d6e95a82bd762f5455369662134cbe3a1260
= <=>
2022-06-10 19:14:09 +0530

    Getting Dirty with JSX and Fragments

--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 function OurApp() {
-  return React.createElement("div", null, [
-    React.createElement("h1", null, "Our Amazing App"),
-    React.createElement("p", null, `The Current Time is ${new Date().toLocaleString()}`),
-    React.createElement("small", null, "Copy right footer Text")
-  ])
-}
+  return /*#__PURE__*/(
+    React.createElement(React.Fragment, null, /*#__PURE__*/
+    React.createElement("h1", { className: "special" }, "Our Amazing React JSX App"), /*#__PURE__*/
+    React.createElement("p", null, "The Current time is ", new Date().toLocaleString()), /*#__PURE__*/
+    React.createElement("small", null, "Copyright Footer Text")));
 
+
+}
 setInterval(() => {
-  ReactDOM.render(React.createElement(OurApp), document.querySelector('#app'))
-}, 1000)
+  ReactDOM.render( /*#__PURE__*/React.createElement(OurApp, null), document.querySelector('#app'));
+}, 1000);
