7c310b3bac793a6f1ad9c65e204eff2833c3b379
= <=>
2022-06-11 00:45:52 +0530

    Component Using Other Component
    Here we turned each line of html to a components and used it to Our App funciton

--- script.js
+++ script.js
@@ -1,12 +1,23 @@
 function OurApp() {
     return (
     <> 
-        <h1 className = "special">Our Amazing React JSX App</h1>
-        <p>The Current time is {new Date().toLocaleString()}</p>
-        <small>Copyright Footer Text</small>
+        <OurHeader />
+        <TimeArea />
+        <Footer />
     </>
     )
 }
+
+function OurHeader() {
+   return (<h1 className = "special">Our Amazing React JSX App</h1>)
+}
+function TimeArea() {
+   return (<p>The Current time is {new Date().toLocaleString()}</p>)
+}
+function Footer() {
+   return (<small>Copyright Footer Text</small>)
+}
+
 setInterval(() => {
 ReactDOM.render(<OurApp />, document.querySelector('#app'));
 }, 1000)
