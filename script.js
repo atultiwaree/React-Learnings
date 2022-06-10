49ec17c7b5decea1fce2eccb5d4257a8262aac84
= <=>
2022-06-11 01:51:15 +0530

    Working with Props
    A props is nothing but properties or say attributes of html that jsx provides it is an object

--- script.js
+++ script.js
@@ -3,9 +3,22 @@ function OurApp() {
     <> 
         <OurHeader />
         <TimeArea />
+        <ul>
+          <Students name = "Atul" work = "Xvideos.com" year = "2020" />
+          <Students name = "Dixit" work = "PornHub.com" year = "2021"/>
+          <Students name = "Faiz" work = "Xnxx.com" year = "2022"/>
+        </ul>
         <Footer />
     </>
     )
+}
+
+function Students(props) {
+     return (
+       <>
+       <li>I'm  {props.name} Working at {props.work} year of joining year {props.year}</li>
+       </>
+     )
 }
 
 function OurHeader() {
