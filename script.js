c4ddbe8caf9c0b875489d67130da59d338d646f5
= <=>
2022-06-11 10:43:08 +0530

    Looping through Array in JSX

--- script.js
+++ script.js
@@ -1,22 +1,29 @@
+const pets = [
+  { name: "Meowsalot", species: "cat", age: "5", id: 123456789 },
+  { name: "Barksalot", species: "dog", age: "3", id: 987654321 },
+  { name: "Fluffy", species: "rabbit", age: "2", id: 123123123 },
+  { name: "Purrsloud", species: "cat", age: "1", id: 456456456 },
+  { name: "Paws", species: "dog", age: "6", id: 789789789 }
+]
+
 function OurApp() {
     return (
     <> 
         <OurHeader />
         <TimeArea />
+      
         <ul>
-          <Students name = "Atul" work = "Xvideos.com" year = "2020" />
-          <Students name = "Dixit" work = "PornHub.com" year = "2021"/>
-          <Students name = "Faiz" work = "Xnxx.com" year = "2022"/>
+ {pets.map(pet => <Pet name = {pet.name} species = {pet.species} age = {pet.age} key = {pet.id} /> )}
         </ul>
         <Footer />
     </>
     )
 }
 
-function Students(props) {
+function Pet(props) {
      return (
        <>
-       <li>I'm  {props.name} Working at {props.work} year of joining year {props.year}</li>
+       <li>I'm  {props.name} a {props.work} of age {props.age}</li>
        </>
      )
 }
