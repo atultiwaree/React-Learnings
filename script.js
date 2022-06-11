89485f168d24fe4f8be4fc4db282e83caa4b9a06
= <=>
2022-06-11 19:23:18 +0530

    Forom Handling Using React

--- script.js
+++ script.js
@@ -1,26 +1,53 @@
 const useState = React.useState
 
-const pets = [
+
+
+function OurApp() {
+  const [pets, setPets] = useState([
   { name: "Meowsalot", species: "cat", age: "5", id: 123456789 },
   { name: "Barksalot", species: "dog", age: "3", id: 987654321 },
   { name: "Fluffy", species: "rabbit", age: "2", id: 123123123 },
   { name: "Purrsloud", species: "cat", age: "1", id: 456456456 },
   { name: "Paws", species: "dog", age: "6", id: 789789789 }
-]
-
-function OurApp() {
+])
     return (
     <> 
         <OurHeader />
         <TimeArea />
         <LikeArea />
-       
+        <AddPetForm setPets = {setPets} />
         <ul>
  {pets.map(pet => <Pet name = {pet.name} species = {pet.species} age = {pet.age} key = {pet.id} /> )}
         </ul>
         <Footer />
     </>
     )
+}
+
+function AddPetForm(props) {
+  const [name, setName] = useState()
+  const [species, setSpecies] = useState()
+  const [age, setAge] = useState()
+  
+  handleSubmit = (e) => {
+      e.preventDefault()
+      props.setPets(prev => prev.concat({name , species , age , id : Date.now()}))
+      setName("")
+      setAge("")
+      setSpecies("")
+  }
+  
+  return (
+    <form onSubmit = {handleSubmit}>
+      <fieldset>
+        <legend>Add New Pet</legend>
+        <input value = {name} onChange = {e => setName(e.target.value)} placeholder="Name" />
+        <input value = {species} onChange = {e => setSpecies(e.target.value)} placeholder="species" />
+        <input value = {age} onChange = {e => setAge(e.target.value)} placeholder="age in years" />
+        <button>Add Pet</button>
+      </fieldset>
+    </form>
+  )
 }
 
 function LikeArea() {
